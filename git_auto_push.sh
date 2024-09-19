ea3f60739b00be9a8fe1146ae0af06df32ab38ca
Evilamajor <evilamajor@uoc.edu>
2024-09-19 10:49:06 +0200

    Mise à jour du script git_auto_push.sh pour gérer les nouveaux fichiers et dossiers

--- git_auto_push.sh
+++ git_auto_push.sh
@@ -7,7 +7,7 @@ PROJECT_DIR=~/Documents/courses
 COMMIT_MESSAGE="Mise à jour automatique du contenu"
 
 # Se déplacer vers le répertoire du projet
-cd $PROJECT_DIR
+cd $PROJECT_DIR || exit
 
 # Vérifier l'état du dépôt Git
 git status
@@ -15,15 +15,25 @@ git status
 # Ajouter tous les nouveaux fichiers, modifications et nouveaux dossiers
 git add .
 
-# Vérifier s'il y a quelque chose à committer
+# Vérifier s'il y a des modifications à committer
 if git diff-index --quiet HEAD --; then
     echo "Aucun changement à committer"
 else
-    # Créer un commit avec un message
-    git commit -m "$COMMIT_MESSAGE"
+    # Demander un message de commit personnalisé à l'utilisateur
+    read -p "Entrez le message de commit (ou appuyez sur Entrée pour utiliser le message par défaut) : " USER_COMMIT_MESSAGE
+
+    # Utiliser le message par défaut si l'utilisateur n'a pas fourni de message personnalisé
+    if [ -z "$USER_COMMIT_MESSAGE" ]; then
+        USER_COMMIT_MESSAGE=$COMMIT_MESSAGE
+    fi
+
+    # Créer un commit avec le message choisi
+    git commit -m "$USER_COMMIT_MESSAGE"
     
     # Pousser les modifications vers la branche principale sur GitHub
     git push origin main
     
-    echo "Modifications poussées vers GitHub"
+    echo "Modifications poussées vers GitHub avec succès"
 fi
+
+
