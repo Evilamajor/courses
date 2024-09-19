7562c1c121d8cf5e34786dfaf7116b05dda01d3d
Evilamajor <evilamajor@uoc.edu>
2024-09-19 10:27:03 +0200

    Ajout du script git_auto_push.sh

---
+++ git_auto_push.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Chemin vers le répertoire de votre projet
+PROJECT_DIR=~/Documents/courses
+
+# Message de commit par défaut (vous pouvez le personnaliser ou le rendre dynamique)
+COMMIT_MESSAGE="Mise à jour automatique du contenu"
+
+# Se déplacer vers le répertoire du projet
+cd $PROJECT_DIR
+
+# Vérifier l'état du dépôt Git
+git status
+
+# Ajouter tous les nouveaux fichiers et modifications à Git
+git add .
+
+# Vérifier s'il y a quelque chose à committer
+if git diff-index --quiet HEAD --; then
+    echo "Aucun changement à committer"
+else
+    # Créer un commit avec un message
+    git commit -m "$COMMIT_MESSAGE"
+    
+    # Pousser les modifications vers la branche principale sur GitHub
+    git push origin main
+    
+    echo "Modifications poussées vers GitHub"
+fi
